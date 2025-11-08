bb3cbc159e5b8e5dc5c377000ac5e9ab28cea441
bijay-develops <bijaybk307@gmail.com>
2025-11-09 01:10:30 +0545

    initialize express app with multer for file uploads and set up routes

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const multer = require("multer");  // Middleware for handling file uploads
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+const port = 4000;
+
+app.set("view engine", "ejs");  // telling express to use ejs as templating engine
+
+app.set("views", path.join(__dirname, "views"));  // setting the folder where .ejs fils are located
+
+// ------------------------------
+// Configure Multer storage engine
+// ------------------------------
+const storage = multer.diskStorage({
+    // define where uploaded files will be stored
+  destination: (req, file, cb) => {
+    cb(null, "filestorage/");  // 'null' indicates no error, and 'filestorage/' is the upload directory
+  },
+  filename: (req, file, cb) => {
+    // define the file naming convention
+    const filename = `${Date.now()}-${file.originalname}`;
+    cb(null, filename);  // 'null' indicates no error, and 'filename' is the new file name
+  },
+});
+
+//Initializing multer with the defined storage engine
+const upload = multer({ storage });
+
+// ------------------------------
+// Serve static files from 'filestorage' directory
+// ------------------------------
+app.use("/uploads", express.static(path.join(__dirname, "filestorage")));   // This allows users to access uploaded files via a URL like /uploads/<filename>
+
+
+// ------------------------------
+// Route: GET /
+// ------------------------------
+// Renders the home page (index.ejs)
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+// ------------------------------
+// Route: POST /upload
+// ------------------------------
+// Handles file uploads from a form input with name="file"
+// Multer's 'upload.single()' middleware processes one file at a time
+app.post('/upload', upload.single('file'), (req, res)=> {
+    res.redirect('/')
+});
+
+// ------------------------------
+// Route: DELETE /delete/:filename
+// ------------------------------
